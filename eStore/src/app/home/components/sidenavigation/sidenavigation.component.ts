0327f01d1e16f454e42e35ef277b18afefb6a769
Mateusz Lewocz <82779@student.pb.edu.pl>
2024-04-13 19:28:56 +0200

    Populate categoires with sample data

--- eStore/src/app/home/components/sidenavigation/sidenavigation.component.ts
+++ eStore/src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Category } from '../../types/category.type';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-sidenavigation',
   standalone: true,
   imports: [],
   templateUrl: './sidenavigation.component.html',
-  styleUrl: './sidenavigation.component.scss'
+  styleUrl: './sidenavigation.component.scss',
 })
 export class SidenavigationComponent {
+  categoreis: Category[] = [];
 
+  constructor(categoryService: CategoryService) {
+    this.categoreis = categoryService.getAllCategories();
+  }
+
+  getCategoires(parent_category_id?: number): Category[] {
+    return this.categoreis.filter((category) => category.parent_category_id === parent_category_id);
+  }
 }
